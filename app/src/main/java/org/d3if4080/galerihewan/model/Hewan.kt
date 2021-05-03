adfbc70268d6d238348f95d45ea4067fb6dc60cd
rafif <rafifsicadelpintar@gmail.com>
2021-05-03 23:50:42 +0700

    3.4.Memproses data internet sebagai JSON

--- app/src/main/java/org/d3if4080/galerihewan/model/Hewan.kt
+++ app/src/main/java/org/d3if4080/galerihewan/model/Hewan.kt
@@ -1,7 +1,9 @@
 package org.d3if4080.galerihewan.model
 
+import org.d3if4080.galerihewan.R
+
 data class Hewan(
     val nama: String,
     val namaLatin: String,
-    val imageResId: Int
+    val imageResId: Int = R.drawable.angsa
 )
